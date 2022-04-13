0ff5af0c84565b38d6521e2d5cd8f8fc7c4b1afe
Jean Porcherot <jean.porcherot@cea.fr>
2022-04-13 08:08:26 +0200

    Add Android platform check

--- edflib.c
+++ edflib.c
@@ -38,7 +38,7 @@
 #define EDFLIB_VERSION  (121)
 #define EDFLIB_MAXFILES  (64)
 
-#if defined(__APPLE__) || defined(__MACH__) || defined(__APPLE_CC__) || defined(__HAIKU__)
+#if defined(__APPLE__) || defined(__MACH__) || defined(__APPLE_CC__) || defined(__HAIKU__) || defined(__ANDROID__)
 
 #define fopeno fopen
 
